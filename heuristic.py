6cabc0f77f094c205dd3d47bbf01643ebe4adc46
spamakin <anakin@icalyx.com>
2023-01-22 23:09:05 -0600

    grid graph generation now takes different granularities

--- heuristic.py
+++ heuristic.py
@@ -7,14 +7,15 @@ def random_points(n, x_max=100.0, y_max=100.0):
     # x coordinates in [0, x_max]
     # y coordinates in [0, y_max]
 
-    coords = [(rd.uniform(0, x_max), rd.uniform(0, y_max)) for _ in range(n + 1)]
-    s = coords[0]
-    targets = coords[1:]
+    # WLOG, s = (0, 0)
+    s = (0, 0)
+
+    targets = [(rd.uniform(0, x_max), rd.uniform(0, y_max)) for _ in range(n)]
 
     return s, targets
 
 
-def form_grid_graph(s, targets, gran):
+def form_grid_graph(s, targets, x_gran, y_gran):
     # gran := total number of vertical and horizontal lines
     #   Maybe specify x and y granularity in future?
     # Add diagonals in one direction if there is not a target
@@ -26,21 +27,25 @@ def form_grid_graph(s, targets, gran):
     y_min = min(y for (_, y) in nodes)
     y_max = max(y for (_, y) in nodes)
 
-    x_gran = (x_max - x_min) / gran
-    y_gran = (y_max - y_min) / gran
+    x_dist = (x_max - x_min) / x_gran
+    y_dist = (y_max - y_min) / y_gran
 
-    G = nx.grid_2d_graph(gran, gran)
+    G = nx.grid_2d_graph(x_gran + 1, y_gran + 1)
 
+    # add distances and set positions of non-start / target nodes
     positions = dict()
-    # add distances
     for x, y in G.nodes():
-        if x < gran - 1:
-            G[x, y][x + 1, y]["weight"] = x_gran
-        if y < gran - 1:
-            G[x, y][x, y + 1]["weight"] = y_gran
+        if x < x_gran:
+            G[x, y][x + 1, y]["weight"] = x_dist
+        if y < y_gran:
+            G[x, y][x, y + 1]["weight"] = y_dist
 
         # set x, y position
-        positions[(x, y)] = (x_min + x * x_gran, y_min + y * y_gran)
-
+        positions[(x, y)] = (x_min + x * x_dist, y_min + y * y_dist)
     nx.set_node_attributes(G, positions, "pos")
+
+    for i, t in enumerate(targets):
+        G.add_node(f"target {i}", pos=t)
+    G.add_node("start", pos=s)
+
     return G
--- testing.py
+++ testing.py
@@ -6,13 +6,23 @@ from heuristic import random_points, form_grid_graph
 from matplotlib import pyplot as plt
 import networkx as nx
 
-s, targets = random_points(5)
-G = form_grid_graph(s, targets, 10)
+n = 5
+s, targets = random_points(n)
+G = form_grid_graph(s, targets, 3*n, 1*n)
 
 positions = dict()
-for x, y in G.nodes():
-    positions[(x, y)] = G.nodes[x, y]["pos"]
+colors = []
+for node in G.nodes():
+    if node == 'start':
+        colors.append('blue')
+
+    elif 'target' in node:
+        colors.append('red')
+    else:
+        colors.append('green')
+
+    positions[node] = G.nodes[node]["pos"]
 
 plt.figure(figsize=(15, 15))
-nx.draw(G, pos=positions, with_labels=True, node_size=800)
+nx.draw(G, pos=positions, node_color=colors, with_labels=False, node_size=200)
 plt.show()
