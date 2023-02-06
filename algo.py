b2a4b617b5870b9f2b08ead43b783a72d48f52b5
Sam Ruggerio <samuelr6@illinois.edu>
2023-02-06 17:35:15 -0600

    improvments and cleanup

--- algo.py
+++ algo.py
@@ -1,29 +1,36 @@
 from util import build_graph
 import networkx as nx
-import sys
-from matplotlib import pyplot as plt
 from benchmark import display_tree
 
-
+# This precomputes SSSP for each target, however we can subsitute with any "heuristic"
+# The heuristic must be a function that ouputs a dictionary of dictionaries of paths
 def compute_SSSP(G, targets):
     # Compute the SSSP for each target using Dijkstra's
     target_paths = dict()
     for target in targets:
-        target_paths[target] = nx.single_source_dijkstra(G, target)
+        target_paths[target] = nx.single_source_dijkstra_path(G, target)
     return target_paths
 
-
-def build_stiener_seed(G, s, targets):
-    # Build the seed MST and trim it.
-    mst = nx.minimum_spanning_tree(G)
-    pred = nx.dfs_predecessors(mst, s)
+def mark_paths(tree, s, targets):
+    pred = nx.dfs_predecessors(tree, s)
+    nx.set_node_attributes(tree, 0, "paths")
 
     # Mark paths from targets towards the source.
     for v in targets:
         while v != s:
-            mst.nodes[v]["paths"] += 1
+            tree.nodes[v]["paths"] += 1
             v = pred[v]
-    mst.nodes[s]["paths"] = len(targets)
+    tree.nodes[s]["paths"] = len(targets)
+
+    # Pass predecessor information back to the caller.
+    return pred
+
+def build_stiener_seed(G, s, targets):
+    # Build the seed MST and trim it.
+    mst = nx.minimum_spanning_tree(G)
+
+    # Mark paths from targets towards the source.
+    pred = mark_paths(mst, s, targets)
 
     # Remove nodes past targets with no further targets.
     remove = []
@@ -34,59 +41,56 @@ def build_stiener_seed(G, s, targets):
     for v in remove:
         mst.remove_node(v)
 
-    return mst
+    return mst, pred
 
 
-def compute_metric(mst, s, targets):
+def compute_metric(mst, s, targets, pred=None):
     # Determine Counterdeception metric, mark if the tree has a forced path.
     # TODO: Define forced path
 
-    pred = nx.dfs_predecessors(mst, s)
+    if pred is None:
+        pred = nx.dfs_predecessors(mst, s)
+
     forced = False
+    # Placeholder value, metric should never match this.
     metric = mst.size(weight="weight")
-    # min_target = None
     target_metrics = []
 
+    # For each target, add up the distance between nodes
+    # Repeat until we reach the source or a node with multiple paths
     for v in targets:
         cur = v
         curdist = 0
         while cur != s and mst.nodes[cur]["paths"] == 1:
-            # print(cur)
-            # print("see node:", mst.nodes[cur])
-            if mst.nodes[cur]["paths"] == 1:
-                curdist += mst.edges[cur, pred[cur]]["weight"]
+            curdist += mst.edges[cur, pred[cur]]["weight"]
             cur = pred[cur]
         if curdist == 0:
             forced = True
         else:
             target_metrics.append((curdist, v))
+    
+    # Sort the target metrics by distance, ascending, and pick the first one.
     target_metrics = sorted(target_metrics)
     metric = target_metrics[0][0]
 
     return forced, metric, target_metrics
 
 
-def reattachment(G, s, targets, budget, mst, forced, metric, target_list):
-    old_metric = metric
+def reattachment(G, s, targets, budget, mst, forced, metric, target_list, pred, target_paths):
     # Pick a target starting with the minimum contribution to the metric distance
-    for dist, v in target_list:
-        pred = nx.dfs_predecessors(mst, s)
-
+    for _, v in target_list:
         # Make a copy of the MST to remove the target and corresponding path from.
         mstprime = mst.copy()
         updated = False
 
         # Remove the target and its path from the tree.
-        toremove = []
         cur = v
         while cur != s and mstprime.nodes[cur]["paths"] == 1:
-            toremove.append(cur)
+            mstprime.remove_node(cur)
             cur = pred[cur]
-        for node in toremove:
-            mstprime.remove_node(node)
 
-        # Compute Dijkstras from the target in the original graph
-        dists, dijpath = nx.single_source_dijkstra(G, v)
+        # Get the path for the target from the precomputed SSSP
+        dijpath = target_paths[v]
 
         # For each node on the remaining tree:
         for potential in mstprime.nodes():
@@ -94,7 +98,6 @@ def reattachment(G, s, targets, budget, mst, forced, metric, target_list):
             path = dijpath[potential]
             # Check if the path crosses any nodes in the tree
             sb = False
-            # print(f"path {path}")
             for x in path[:-1]:
                 if x in mstprime.nodes():
                     sb = True
@@ -103,7 +106,7 @@ def reattachment(G, s, targets, budget, mst, forced, metric, target_list):
 
             # Make a new tree to reattach the target to.
             mstcheck = mstprime.copy()
-            # Add node and path to the tree
+            # Add nodes and edges to the tree.
             for i in range(len(path) - 1):
                 mstcheck.add_node(path[i], paths=1, pos=G.nodes[path[i]]["pos"])
                 if path[i + 1] not in mstcheck.nodes():
@@ -113,19 +116,14 @@ def reattachment(G, s, targets, budget, mst, forced, metric, target_list):
                     path[i + 1],
                     weight=G.edges[path[i], path[i + 1]]["weight"],
                 )
-
-            predcheck = nx.dfs_predecessors(mstcheck, s)
-            nx.set_node_attributes(mstcheck, 0, "paths")
-            for v in targets:
-                while v != s:
-                    mstcheck.nodes[v]["paths"] += 1
-                    v = predcheck[v]
-            mstcheck.nodes[s]["paths"] = len(targets)
-            # print("here")
-            forcedp, metricp, target_listp = compute_metric(mstcheck, s, targets)
+            # Compute the new predecessor list and metric on the tree.
+            predcheck = mark_paths(mstcheck, s, targets)
+            forcedp, metricp, target_listp = compute_metric(mstcheck, s, targets, predcheck)
+            # If the tree either removes forced paths or improves the metric w/o adding forced paths,
+            # *and* the tree is under the budget, update the tree and corresponding values. 
             if (
-                forcedp == False
-                and forced == True
+                (forcedp == False
+                and forced == True)
                 or (metricp > metric and forcedp == forced)
             ):
                 if mstcheck.size(weight="weight") < budget:
@@ -145,40 +143,40 @@ def reattachment(G, s, targets, budget, mst, forced, metric, target_list):
                     metric = metricp
                     target_list = target_listp
                     updated = True
-
-                    # display_tree(G, mst)
-
+                    pred = predcheck
                     break
+        # Don't try to reattach any other targets if we updated the tree.
+        # The same or earlier targets may be reattached multiple times.
+        # Note that if we change "reattaching the minimum target", this condition may need to change
         if updated:
             break
 
-    return mst, forced, metric, target_list
+
+    return mst, forced, metric, target_list, pred
 
 
-def algorithm(G, s, targets, budget):
-    # This needs a better name
-
-    mst = build_stiener_seed(G, s, targets)
+def reattachment_approximation(G, s, targets, budget):
+    # Build the seed MST and trim it.
+    mst, pred = build_stiener_seed(G, s, targets)
     mstbench = mst.copy()
-
-    forced, metric, target_list = compute_metric(mst, s, targets)
+    forced, metric, target_list = compute_metric(mst, s, targets, pred)
     metricbench = metric
 
+    # Precompute Dijkstra's from each target to all other nodes in the graph
+    target_paths = compute_SSSP(G, targets)
+
+    # Get original characteristics of the tree
     originalsize = mst.size(weight="weight")
     print("FORCED: ", forced)
 
     display_tree(G, mst)
-    # print(nx.single_source_dijkstra(G, s))
 
-    old_metric = metric + 1
-    curcost = mst.size(weight="weight")
-    # print(mst.nodes())
-
+    old_metric = None
     # Continue until we find no local improvement
     while old_metric != metric:
         old_metric = metric
-        mst, forced, metric, target_list = reattachment(
-            G, s, targets, budget, mst, forced, metric, target_list
+        mst, forced, metric, target_list, pred = reattachment(
+            G, s, targets, budget, mst, forced, metric, target_list, pred, target_paths
         )
 
     print(
@@ -198,8 +196,8 @@ def main():
 
     # Set up graph, seed tree, and metric values.
     G, s, targets = build_graph(count, graphx, graphy)
-    budget = build_stiener_seed(G, s, targets).size(weight="weight") * 3
-    algorithm(G, s, targets, budget)
+    budget = build_stiener_seed(G, s, targets)[0].size(weight="weight") * 3
+    reattachment_approximation(G, s, targets, budget)
 
 
 if __name__ == "__main__":
