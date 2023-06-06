f2f02d50de2139d8de3a7ca700f7d409893d3087
spamakin <anakin@icalyx.com>
2023-06-05 23:39:29 -0500

    bruteforce computation revamp

--- bruteforce.py
+++ bruteforce.py
@@ -1,69 +1,90 @@
 import networkx as nx
+from networkx.algorithms.tree.mst import SpanningTreeIterator
+from algo import compute_metric, mark_paths
+import pickle
 
-from util import random_graph
+from util import random_graph, display_tree, display_graph, random_points, form_grid_graph, round_targets_to_graph, bcolors
 
-V = 5
-tcount = 2
-G, s, targets = random_graph(V, tcount)
-COST = nx.minimum_spanning_tree(G, weight="weight").size(weight="weight") * 1.5
+def bruteforce(G, s, targets, budget):
+    best = float('-inf')
+    best_tree = None
+    count = 0
+    spacer = 100
+    for t in SpanningTreeIterator(G):
+        if count % spacer == 0:
+            print(f"Num Trees: {bcolors.OKBLUE}{count}{bcolors.ENDC}")
+            print(f"Metric: {bcolors.OKBLUE}{best}{bcolors.ENDC}")
+        count += 1
 
-print("start: ", s, "targets: ", targets)
-best = None
-bestc = float("inf")
-bestm = 0
-for t in nx.SpanningTreeIterator(G, weight="weight", minimum=True):
-    # Get Predecessors
-    pred = nx.dfs_predecessors(t, s)
+        # trim tree
+        # Mark paths from targets towards the source.
+        pred = mark_paths(t, s, targets)
 
-    # Determine Paths Counts
-    for v in targets:
-        while v != s:
-            t.nodes[v]["paths"] += 1
-            v = pred[v]
-    t.nodes[s]["paths"] = tcount
+        # Remove nodes past targets with no further targets.
+        remove = []
+        for v in t.nodes():
+            if t.nodes[v]["paths"] == 0:
+                remove.append(v)
 
-    # Remove Nodes with no paths
-    remove = []
-    for v in t.nodes():
-        if t.nodes[v]["paths"] == 0:
-            remove.append(v)
+        for v in remove:
+            t.remove_node(v)
 
-    for v in remove:
-        t.remove_node(v)
+        # janky early return
+        if t.size(weight="weight") > budget:
+            print(bcolors.CLEAR_LAST_LINE)
+            return best_tree
 
-    c = t.size(weight="weight")
-    if c >= COST:
-        continue
+        # compute metric
+        forced, metric, target_metrics = compute_metric(t, s, targets, pred=pred)
 
-    # Determine Counterdeception metric
-    metric = float("inf")
-    forced = False
+        metric = metric if not forced else 0.0
+        if metric > best:
+            best = metric
+            best_tree = t
 
-    for v in targets:
-        cur = v
-        curdist = 0
-        while cur != s and t.nodes[cur]["paths"] == 1:
-            if t.nodes[cur]["paths"] == 1:
-                curdist += t.edges[cur, pred[cur]]["weight"]
-            cur = pred[cur]
-        if curdist == 0:
-            forced = True
-        else:
-            metric = min(metric, curdist)
+        if count % spacer == spacer - 1:
+            print(f"{bcolors.CLEAR_LAST_LINE}")
+            print(f"{bcolors.CLEAR_LAST_LINE}")
 
-    if forced:
-        metric = 0.0
-    else:
-        if metric > bestm:
-            best = t
-            bestc = c
-            bestm = metric
-        elif metric == bestm:
-            if c < bestc:
-                best = t
-                bestc = c
-                bestm = metric
+    # if we don't early return, we've explored everything
+    return best_tree
 
-    print(t, "weight: ", c, "/", COST, "metric: ", metric, "forced: ", forced)
+def main():
+    # Initial Parameters
+    target_count = 2
+    graphx = 2
+    graphy = 2
 
-print("Best Tree: ", best, "weight: ", bestc, "/", COST, "metric: ", bestm)
+    def factory():
+        s, targets = random_points(target_count)
+
+        # G = form_grid_graph(s, targets, graphx, graphy)
+        G = form_grid_graph(s, targets, graphx, graphy, triangulate=False)
+        # G = form_hex_graph(s, targets, graphx, graphy, 1.0)
+        # G = form_triangle_graph(s, targets, graphx, graphy, 1.0)
+
+        round_targets_to_graph(G, s, targets)
+        targets = [f"target {i}" for i in range(target_count)]
+        s = "start"
+        nx.set_node_attributes(G, 0, "paths")
+
+        budget = float("inf")
+        # budget = nx.minimum_spanning_tree(G).size(weight="weight") * 0.5
+
+        # rescale weights
+        for u, v in G.edges:
+            G[u][v]["weight"] = G[u][v]["weight"]
+
+        return G, s, targets, budget
+
+    prefix = "results/brute/"
+    for i in range(3):
+        G, s, targets, budget = factory()
+        best_tree = bruteforce(G, s, targets, budget)
+
+        pickle.dump(G, open(f"{prefix}G_{i + 1}.pickle", 'wb'))
+        pickle.dump(best_tree, open(f"{prefix}best_tree_{i + 1}.pickle", 'wb'))
+        print()
+
+if __name__ == "__main__":
+    main()
