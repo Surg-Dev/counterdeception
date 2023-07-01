b958fa5aa21be3d2ba68b266940beeb203a1be1e
spamakin <anakin@icalyx.com>
2023-07-01 17:37:20 -0500

    bruteforce read

--- bruteforce.py
+++ bruteforce.py
@@ -16,6 +16,7 @@ from util import (
 )
 
 # number n you put here = a(n)
+#  Number of spanning trees in n x n grid
 # https://oeis.org/A007341
 a = [
     1,
@@ -101,6 +102,9 @@ def generate_bruteforce_graphs(factory, count, prefix=None):
                 "time": ellapsed,
                 "metric": best,
             }
+            for k, v in info.items():
+                print(k, v)
+            print()
             pickle.dump(info, open(f"{prefix}/{i + 1}/info.pickle", "wb"))
         else:
             display_tree(G, best_tree)
@@ -112,7 +116,11 @@ def generate_bruteforce_graphs(factory, count, prefix=None):
             "total_time": ellapsed,
             "avg_time": ellapsed / count,
         }
+        for k, v in stats.items():
+            print(k, v)
+        print()
         pickle.dump(stats, open(f"{prefix}/stats.pickle", "wb"))
+
 
 
 def sample_algo(prefix, count, rounds):
@@ -254,63 +262,63 @@ def time_random(n, factory, count, samples):
 
 
 def main():
-    # Goal: Show that random benchmarking does not scale well
-    #   Plot n by # of spanning trees in n x n graph / # number of random trees we can generate in that time
+    # # Goal: Show that random benchmarking does not scale well
+    # #   Plot n by # of spanning trees in n x n graph / # number of random trees we can generate in that time
 
-    # number of graphs to generate
-    count = 2
-    # number of random graphs to bruteforce
-    samples = 10000
+    # # number of graphs to generate
+    # count = 2
+    # # number of random graphs to bruteforce
+    # samples = 10000
 
-    # graph parameters
-    target_count = 2
-    n_min, n_max = 3, 6
+    # # graph parameters
+    # target_count = 2
+    # n_min, n_max = 3, 6
 
-    def factory(n):
-        s, targets = random_points(target_count)
+    # def factory(n):
+    #     s, targets = random_points(target_count)
 
-        G = form_grid_graph(s, targets, n, n, triangulate=False)
+    #     G = form_grid_graph(s, targets, n, n, triangulate=False)
 
-        round_targets_to_graph(G, s, targets)
-        targets = [f"target {i}" for i in range(target_count)]
-        s = "start"
-        nx.set_node_attributes(G, 0, "paths")
+    #     round_targets_to_graph(G, s, targets)
+    #     targets = [f"target {i}" for i in range(target_count)]
+    #     s = "start"
+    #     nx.set_node_attributes(G, 0, "paths")
 
-        budget = float("inf")
+    #     budget = float("inf")
 
-        # rescale weights
-        for u, v in G.edges:
-            G[u][v]["weight"] = G[u][v]["weight"]
+    #     # rescale weights
+    #     for u, v in G.edges:
+    #         G[u][v]["weight"] = G[u][v]["weight"]
 
-        return G, s, targets, budget
+    #     return G, s, targets, budget
 
-    brute_times = [
-        time_bruteforce(n, factory, count, samples) / samples
-        for n in range(n_min, n_max + 1)
-    ]
-    extrapolated_bruteforce = [
-        brute_times[i] * a[n_min + i] for i in range(len(brute_times))
-    ]
+    # brute_times = [
+    #     time_bruteforce(n, factory, count, samples) / samples
+    #     for n in range(n_min, n_max + 1)
+    # ]
+    # extrapolated_bruteforce = [
+    #     brute_times[i] * a[n_min + i] for i in range(len(brute_times))
+    # ]
 
-    rand_times = [
-        time_random(n, factory, count, samples) / samples
-        for n in range(n_min, n_max + 1)
-    ]
-    extrapolated_random = [rand_times[i] * a[n_min + i] for i in range(len(rand_times))]
+    # rand_times = [
+    #     time_random(n, factory, count, samples) / samples
+    #     for n in range(n_min, n_max + 1)
+    # ]
+    # extrapolated_random = [rand_times[i] * a[n_min + i] for i in range(len(rand_times))]
 
-    print("Extrapolated Times")
-    for i, (extrap_time, rand_time) in enumerate(
-        zip(extrapolated_bruteforce, extrapolated_random)
-    ):
-        print(f"    {n_min + i + 1} x {n_min + i + 1} nodes")
-        print(f"        Number of Graphs to Iterate Through: {a[n_min + i]}")
-        print(f"        Bruteforce Time: {extrap_time}s")
-        print(f"        Bruteforce Time: {extrap_time / 3600}h")
-        print(f"        Random Time:     {rand_time}s")
-        print(f"        Random Time:     {rand_time / 3600}h")
-        print()
+    # print("Extrapolated Times")
+    # for i, (extrap_time, rand_time) in enumerate(
+    #     zip(extrapolated_bruteforce, extrapolated_random)
+    # ):
+    #     print(f"    {n_min + i + 1} x {n_min + i + 1} nodes")
+    #     print(f"        Number of Graphs to Iterate Through: {a[n_min + i]}")
+    #     print(f"        Bruteforce Time: {extrap_time}s")
+    #     print(f"        Bruteforce Time: {extrap_time / 3600}h")
+    #     print(f"        Random Time:     {rand_time}s")
+    #     print(f"        Random Time:     {rand_time / 3600}h")
+    #     print()
 
-    # TODO: Put all of this into individual functions
+    # # TODO: Put all of this into individual functions
 
     ##################################
     # GENERATE GRAPHS AND BRUTEFORCE #
@@ -353,94 +361,94 @@ def main():
 
     generate_bruteforce_graphs(factory, n, prefix=loc)
 
-    ########################################
-    # GET TIMINGS FOR ALGORITHM AND RANDOM #
-    ########################################
+    # ########################################
+    # # GET TIMINGS FOR ALGORITHM AND RANDOM #
+    # ########################################
 
-    stats = pickle.load(open(f"{loc}/stats.pickle", "rb"))
+    # stats = pickle.load(open(f"{loc}/stats.pickle", "rb"))
 
-    # budget in seconds
-    allotted = stats["avg_time"]
-    count = stats["count"]
+    # # budget in seconds
+    # allotted = stats["avg_time"]
+    # count = stats["count"]
 
-    samples = 10000
-    spacer = 100
+    # samples = 10000
+    # spacer = 100
 
-    algo_time = 0.0
-    for i in range(samples):
-        if i % spacer == 0:
-            print(f"Num Sampless: ~{bcolors.OKBLUE}{i}{bcolors.ENDC}")
-        G, s, targets, budget = factory()
+    # algo_time = 0.0
+    # for i in range(samples):
+    #     if i % spacer == 0:
+    #         print(f"Num Sampless: ~{bcolors.OKBLUE}{i}{bcolors.ENDC}")
+    #     G, s, targets, budget = factory()
 
-        start_time = time.perf_counter()
-        res, _, _ = compute_tree(G, s, targets, budget)
-        forced, metric, _ = compute_metric(res, s, targets)
-        end_time = time.perf_counter()
+    #     start_time = time.perf_counter()
+    #     res, _, _ = compute_tree(G, s, targets, budget)
+    #     forced, metric, _ = compute_metric(res, s, targets)
+    #     end_time = time.perf_counter()
 
-        algo_time += end_time - start_time
-        if i % spacer == spacer - 1:
-            print(f"{bcolors.CLEAR_LAST_LINE}")
-    algo_time /= samples
+    #     algo_time += end_time - start_time
+    #     if i % spacer == spacer - 1:
+    #         print(f"{bcolors.CLEAR_LAST_LINE}")
+    # algo_time /= samples
 
-    rounds = int(allotted / algo_time)
+    # rounds = int(allotted / algo_time)
 
-    stats["algo_time"] = algo_time
-    stats["algo_rounds"] = rounds
+    # stats["algo_time"] = algo_time
+    # stats["algo_rounds"] = rounds
 
-    sample_algo(loc, count, rounds)
+    # sample_algo(loc, count, rounds)
 
-    print("Doing Random Sampling")
-    rand_time = 0.0
-    for i in range(samples):
-        if i % spacer == spacer - 1:
-            print(f"{bcolors.CLEAR_LAST_LINE}")
-        if i % spacer == 0:
-            print(f"Num Samples: ~{bcolors.OKBLUE}{i}{bcolors.ENDC}")
-        G, s, targets, budget = factory()
+    # print("Doing Random Sampling")
+    # rand_time = 0.0
+    # for i in range(samples):
+    #     if i % spacer == spacer - 1:
+    #         print(f"{bcolors.CLEAR_LAST_LINE}")
+    #     if i % spacer == 0:
+    #         print(f"Num Samples: ~{bcolors.OKBLUE}{i}{bcolors.ENDC}")
+    #     G, s, targets, budget = factory()
 
-        start_time = time.perf_counter()
-        size = float("inf")
-        while size > budget or size == float("inf"):
-            rst, pred = build_stiener_seed(G, s, targets, minimum=None)
-            size = rst.size(weight="weight")
-        forced, metric, _ = compute_metric(rst, s, targets, pred=pred)
-        end_time = time.perf_counter()
+    #     start_time = time.perf_counter()
+    #     size = float("inf")
+    #     while size > budget or size == float("inf"):
+    #         rst, pred = build_stiener_seed(G, s, targets, minimum=None)
+    #         size = rst.size(weight="weight")
+    #     forced, metric, _ = compute_metric(rst, s, targets, pred=pred)
+    #     end_time = time.perf_counter()
 
-        rand_time += end_time - start_time
-    rand_time /= samples
+    #     rand_time += end_time - start_time
+    # rand_time /= samples
 
-    rounds = int(allotted / rand_time)
+    # rounds = int(allotted / rand_time)
 
-    stats["rand_time"] = rand_time
-    stats["rand_rounds"] = rounds
+    # stats["rand_time"] = rand_time
+    # stats["rand_rounds"] = rounds
 
-    sample_rand(loc, count, rounds)
+    # sample_rand(loc, count, rounds)
 
-    pickle.dump(stats, open(f"{loc}/stats.pickle", "wb"))
+    # pickle.dump(stats, open(f"{loc}/stats.pickle", "wb"))
 
-    ###################
-    # GET FINAL STATS #
-    ###################
+    # ###################
+    # # GET FINAL STATS #
+    # ###################
 
-    stats = pickle.load(open(f"{loc}/stats.pickle", "rb"))
-    for k, v in stats.items():
-        print(f"{k}: {v}")
-    print()
+    # stats = pickle.load(open(f"{loc}/stats.pickle", "rb"))
+    # for k, v in stats.items():
+    #     print(f"{k}: {v}")
+    # print()
 
-    for i in range(count):
-        if os.path.exists(f"{loc}/{i + 1}/info.pickle"):
-            brute_res = pickle.load(open(f"{loc}/{i + 1}/info.pickle", "rb"))
-            print(f"    Bruteforce Metric: {brute_res['metric']}")
+    # for i in range(count):
+    #     if os.path.exists(f"{loc}/{i + 1}/info.pickle"):
+    #         brute_res = pickle.load(open(f"{loc}/{i + 1}/info.pickle", "rb"))
+    #         print(f"    Bruteforce Metric: {brute_res['metric']}")
 
-        if os.path.exists(f"{loc}/{i + 1}/algo_res.pickle"):
-            algo_res = pickle.load(open(f"{loc}/{i + 1}/algo_res.pickle", "rb"))
-            print(f"    Algorithm Metric:  {algo_res['metric']}")
+    #     if os.path.exists(f"{loc}/{i + 1}/algo_res.pickle"):
+    #         algo_res = pickle.load(open(f"{loc}/{i + 1}/algo_res.pickle", "rb"))
+    #         print(f"    Algorithm Metric:  {algo_res['metric']}")
 
-        if os.path.exists(f"{loc}/{i + 1}/rand_res.pickle"):
-            rand_res = pickle.load(open(f"{loc}/{i + 1}/rand_res.pickle", "rb"))
-            print(f"    Random Metric:     {rand_res['metric']}")
+    #     if os.path.exists(f"{loc}/{i + 1}/rand_res.pickle"):
+    #         rand_res = pickle.load(open(f"{loc}/{i + 1}/rand_res.pickle", "rb"))
+    #         print(f"    Random Metric:     {rand_res['metric']}")
 
-        print()
+    #     print()
 
 
 if __name__ == "__main__":
