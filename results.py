d7c071227d38e1c433d962243f68fc0cc4811225
Spamakin <60445095+Spamakin@users.noreply.github.com>
2023-09-04 04:22:00 +0000

    some changes in results

--- results.py
+++ results.py
@@ -20,7 +20,7 @@ from util import (
     display_graph,
     display_tree,
 )
-import cv2
+# import cv2
 from bruteforce import generate_bruteforce_graphs, num_span
 
 
@@ -719,64 +719,66 @@ def main():
     #            )
     # plt.show()
 
-    # ### Compute and time reattachment ###
+    ### Compute and time reattachment ###
 
-    # loc = "results/real"
-    # G_f = open(f"{loc}/G.pickle", "rb")
-    # G = pickle.load(G_f)
-    # info_f = open(f"{loc}/info.pickle", "rb")
-    # info = pickle.load(info_f)
-    # s = info["s"]
-    # targets = info["targets"]
-    # G_f.close()
-    # info_f.close()
+    loc = "results/real"
+    G_f = open(f"{loc}/G.pickle", "rb")
+    G = pickle.load(G_f)
+    info_f = open(f"{loc}/info.pickle", "rb")
+    info = pickle.load(info_f)
+    s = info["s"]
+    targets = info["targets"]
+    budget = info["budget"]
+    G_f.close()
+    info_f.close()
 
-    # print("Starting Reattachment...")
-    # start = time.perf_counter()
-    # res, pred, rounds = compute_tree(G, s, targets, budget, loc=f"{loc}/gen")
-    # end = time.perf_counter()
+    print("Starting Reattachment...")
+    start = time.perf_counter()
+    res, pred, rounds = compute_tree(G, s, targets, budget, loc=f"{loc}/gen")
+    end = time.perf_counter()
+    print("Elapsed Time =", end - start)
+    
+    # ### Compute iterative results ###
+    # metric_res = []
+    # mask = cv2.rotate(mask, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    # for i in range(rounds):
+    #     print(f"Creating Tree {i + 1}")
+    #     curr_f = open(f"{loc}/gen/{i}.pickle", "rb")
+    #     curr = pickle.load(curr_f)
+    #     curr_f.close()
 
-    # Compute iterative results
-    metric_res = []
-    mask = cv2.rotate(mask, cv2.ROTATE_90_COUNTERCLOCKWISE)
-    for i in range(rounds):
-        print(f"Creating Tree {i + 1}")
-        curr_f = open(f"{loc}/gen/{i}.pickle", "rb")
-        curr = pickle.load(curr_f)
-        curr_f.close()
+    #     # stats
+    #     forced, metric, _ = compute_metric(curr, s, targets)
+    #     metric_res.append(metric)
 
-        # stats
-        forced, metric, _ = compute_metric(curr, s, targets)
-        metric_res.append(metric)
-
-        fig = plt.figure(frameon=False, figsize=(10, 19))
-        extent = 0, img.shape[1], 0, img.shape[0]
-        plt.imshow(mask, extent=extent, interpolation="nearest")
-        nodes = curr.nodes(data=True)
-        colors = []
-        sizes = []
-        for node in curr.nodes():
-            if node == "start":
-                colors.append("blue")
-                sizes.append(15)
-            elif "target" in node:
-                colors.append("red")
-                sizes.append(15)
-            else:
-                colors.append("green")
-                sizes.append(4)
-        positions = nx.get_node_attributes(G, "pos")
-        nx.draw(
-            curr,
-            pos=positions,
-            node_size=sizes,
-            node_color=colors,
-        )
-        plt.show()
-        plt.savefig(f"{loc}/pics/{i}.png")
-        plt.close()
-    for metric in metric_res:
-        print(metric)
+    #     fig = plt.figure(frameon=False, figsize=(10, 19))
+    #     extent = 0, img.shape[1], 0, img.shape[0]
+    #     plt.imshow(mask, extent=extent, interpolation="nearest")
+    #     nodes = curr.nodes(data=True)
+    #     colors = []
+    #     sizes = []
+    #     for node in curr.nodes():
+    #         if node == "start":
+    #             colors.append("blue")
+    #             sizes.append(15)
+    #         elif "target" in node:
+    #             colors.append("red")
+    #             sizes.append(15)
+    #         else:
+    #             colors.append("green")
+    #             sizes.append(4)
+    #     positions = nx.get_node_attributes(G, "pos")
+    #     nx.draw(
+    #         curr,
+    #         pos=positions,
+    #         node_size=sizes,
+    #         node_color=colors,
+    #     )
+    #     plt.show()
+    #     plt.savefig(f"{loc}/pics/{i}.png")
+    #     plt.close()
+    # for metric in metric_res:
+    #     print(metric)
 
     ####################
     # BUDGET BENCHMARK #
