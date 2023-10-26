f3f89d66e17ca4ed0ca18d4bb1f7286c052a77ab
spamakin <60445095+Spamakin@users.noreply.github.com>
2023-10-26 00:03:40 -0500

    redid some examples

--- examples.py
+++ examples.py
@@ -734,6 +734,219 @@ def dag(loc=None):
     else:
         plt.show()
 
+def reattach_after(loc=None):
+    # Before of Reattachment
+
+    # form graph
+    s = (0, 2)
+    targets = [(0, 0), (4, 4)]
+    G = nx.grid_2d_graph(5, 5)
+    positions = {(x, y): (x, y) for x, y in G.nodes()}
+    nx.set_node_attributes(G, positions, "pos")
+    nx.relabel_nodes(G, {s: "start"}, copy=False)
+    for i, tar in enumerate(targets):
+        nx.relabel_nodes(G, {tar: f"target {i}"}, copy=False)
+
+    # specify edges to keep
+    t = G.to_directed()
+    blue_edges = {
+        ((1, 3), (1, 2)),
+        ((1, 2), (1, 1)),
+        ((1, 1), (0, 1)),
+        ((0, 1), "target 0"),
+        ((1, 3), (2, 3)),
+        ((2, 3), (3, 3)),
+        ((3, 3), (4, 3)),
+        ((4, 3), "target 1"),
+    }
+    black_edges = {
+        ("start", (0, 3)),
+        ((0, 3), (1, 3))
+    }
+    keep = blue_edges | black_edges
+    for edge in list(t.edges()):
+        if edge not in keep:
+            t.remove_edge(*edge)
+
+    # label nodes
+    labels = dict()
+    for node in t.nodes():
+        if "start" not in node and "target" not in node:
+            labels[node] = ""
+        else:
+            labels[node] = node
+
+    # set node attributes
+    colors = []
+    sizes = []
+    for node in t.nodes():
+        if node == "start":
+            colors.append(START_COLOR)
+            sizes.append(HIGHLIGHT_SIZE)
+        elif "target" in node:
+            colors.append(TARGET_COLOR)
+            sizes.append(HIGHLIGHT_SIZE)
+        elif node == (1, 3):
+            colors.append(HIGHLIGHT_COLOR)
+            sizes.append(HIGHLIGHT_SIZE)
+        else:
+            colors.append(NODE_COLOR)
+            sizes.append(NODE_SIZE)
+
+    # set edge attributes
+    weights = []
+    edge_colors = []
+    styles = []
+    for e in t.edges():
+        weights.append(5)
+        if e in blue_edges:
+            edge_colors.append("blue")
+            styles.append("solid")
+        elif e in black_edges:
+            edge_colors.append("black")
+            styles.append("solid")
+        else:
+            edge_colors.append("white")
+            styles.append("solid")
+
+    # start drawing
+    plt.figure(figsize=(10, 10))
+    positions = nx.get_node_attributes(G, "pos")
+    nx.draw(
+        t,
+        pos=positions,
+        node_color=colors,
+        node_size=sizes,
+        width=weights,
+        arrowsize=80,
+        arrowstyle="->",
+        edge_color=edge_colors,
+        style=styles,
+    )
+
+    # move labels
+    for node in positions:
+        x, y = positions[node]
+        if "target 0" in node:
+            positions[node] = (x + 0.60, y + 0.30)
+        elif "target 1" in node:
+            positions[node] = (x - 0.20, y + 0.30)
+        elif "start" in node:
+            positions[node] = (x + 0.40, y)
+        else:
+            positions[node] = (x, y)
+
+    # finish drawing
+    nx.draw_networkx_labels(G, positions, labels, font_size=35)
+    if loc != None:
+        print(f"saving {loc}")
+        plt.savefig(loc)
+        plt.close()
+    else:
+        plt.show()
+
+def reattach_before(loc=None):
+    # Before of Reattachment
+
+    # form graph
+    s = (0, 2)
+    targets = [(0, 0), (4, 4)]
+    G = nx.grid_2d_graph(5, 5)
+    positions = {(x, y): (x, y) for x, y in G.nodes()}
+    nx.set_node_attributes(G, positions, "pos")
+    nx.relabel_nodes(G, {s: "start"}, copy=False)
+    for i, tar in enumerate(targets):
+        nx.relabel_nodes(G, {tar: f"target {i}"}, copy=False)
+
+    # specify edges to keep
+    t = G.to_directed()
+    blue_edges = {
+        ("start", (0, 1)),
+        ((0, 1), "target 0"),
+        ("start", (0, 3)),
+        ((0, 3), (1, 3)),
+        ((1, 3), (2, 3)),
+        ((2, 3), (3, 3)),
+        ((3, 3), (4, 3)),
+        ((4, 3), "target 1")
+    }
+    keep = blue_edges
+    for edge in list(t.edges()):
+        if edge not in keep:
+            t.remove_edge(*edge)
+
+    # label nodes
+    labels = dict()
+    for node in t.nodes():
+        if "start" not in node and "target" not in node:
+            labels[node] = ""
+        else:
+            labels[node] = node
+
+    # set node attributes
+    colors = []
+    sizes = []
+    for node in t.nodes():
+        if node == "start":
+            colors.append(START_COLOR)
+            sizes.append(HIGHLIGHT_SIZE)
+        elif "target" in node:
+            colors.append(TARGET_COLOR)
+            sizes.append(HIGHLIGHT_SIZE)
+        else:
+            colors.append(NODE_COLOR)
+            sizes.append(NODE_SIZE)
+
+    # set edge attributes
+    weights = []
+    edge_colors = []
+    styles = []
+    for e in t.edges():
+        weights.append(5)
+        if e in blue_edges:
+            edge_colors.append("blue")
+            styles.append("solid")
+        else:
+            edge_colors.append("white")
+            styles.append("solid")
+
+    # start drawing
+    plt.figure(figsize=(10, 10))
+    positions = nx.get_node_attributes(G, "pos")
+    nx.draw(
+        t,
+        pos=positions,
+        node_color=colors,
+        node_size=sizes,
+        width=weights,
+        arrowsize=80,
+        arrowstyle="->",
+        edge_color=edge_colors,
+        style=styles,
+    )
+
+    # move labels
+    for node in positions:
+        x, y = positions[node]
+        if "target 0" in node:
+            positions[node] = (x + 0.60, y + 0.30)
+        elif "target 1" in node:
+            positions[node] = (x - 0.20, y + 0.30)
+        elif "start" in node:
+            positions[node] = (x + 0.40, y)
+        else:
+            positions[node] = (x, y)
+
+    # finish drawing
+    nx.draw_networkx_labels(G, positions, labels, font_size=35)
+    if loc != None:
+        print(f"saving {loc}")
+        plt.savefig(loc)
+        plt.close()
+    else:
+        plt.show()
+
+
 def reattachment(loc=None):
 
     # form graph
@@ -796,11 +1009,17 @@ def main():
     # filename = "final_results/examples/reattach"
     # reattachment(loc=filename)
 
-    filename = "final_results/examples/naive_max"
-    naive_strats_longest(loc=filename)
+    filename = "final_results/examples/reattach_before"
+    reattach_before(loc=filename)
 
-    filename = "final_results/examples/naive_min"
-    naive_strats_shortest(loc=filename)
+    filename = "final_results/examples/reattach_after"
+    reattach_after(loc=filename)
+
+    # filename = "final_results/examples/naive_max"
+    # naive_strats_longest(loc=filename)
+
+    # filename = "final_results/examples/naive_min"
+    # naive_strats_shortest(loc=filename)
 
 if __name__ == "__main__":
     main()
